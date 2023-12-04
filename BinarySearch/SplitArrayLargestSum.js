129bd3da7b3cfd3f7ad823ec20ca4314812cd6f9
pwsd21 <pawansachdeva1998@gmail.com>
2023-12-04 12:33:52 +0530

    Split Array Largest Sum - Minimize

--- BinarySearch/SplitArrayLargestSum.js
+++ BinarySearch/SplitArrayLargestSum.js
@@ -1,3 +1,30 @@
-const splitArray = (arr, k) => {};
+const canBeSplit = (nums, maximumSum, k) => {
+  let currSum = 0,
+    parts = 1;
+  for (const num of nums) {
+    currSum += num;
+    if (currSum > maximumSum) {
+      currSum = num;
+      parts++;
+    }
+    if (parts > k) {
+      return false;
+    }
+  }
+  return true;
+};
 
+const splitArray = (nums, k) => {
+  let start = Math.max(...nums),
+    end = nums.reduce((a, b) => a + b);
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    if (canBeSplit(nums, mid, k)) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return start;
+};
 console.log(splitArray([7, 2, 5, 10, 8], 2));
