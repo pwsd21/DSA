16a4758c42807b46e0a815e73e3e5fbc4ad55d22
pwsd21 <pawansachdeva1998@gmail.com>
2024-05-27 14:34:18 +0530

    Flatten Object

---
+++ Lowes/RSC_PracticeQuestions/FlattenObject.js
@@ -0,0 +1,18 @@
+const flattenObject = (obj, parentKey = "", result = {}) => {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const newKey = parentKey ? `${parentKey}.${key}` : key;
+
+      if (
+        typeof obj[key] === "object" &&
+        obj[key] !== null &&
+        !Array.isArray(obj[key])
+      ) {
+        flattenObject(obj[key], newKey, result);
+      } else {
+        result[newKey] = obj[key];
+      }
+    }
+  }
+  return result;
+};
